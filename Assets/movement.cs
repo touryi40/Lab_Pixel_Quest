968252baea291b85d2d9c26b3c213936e72eceac
touryi40 <touryi2000@gmail.com>
2025-07-10 11:33:33 -0400

    code 7/10

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,31 +4,33 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    
+    private Rigidbody2D rb;
+    public int speed = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb= GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.position += new Vector3(0, 1, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.position += new Vector3(0, -1, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.position += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.position += new Vector3(1, 0, 0);
-        }
+        rb.velocity = Vector2.left;
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    transform.position += new Vector3(0, 1, 0);
+        //}
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    transform.position += new Vector3(0, -1, 0);
+        //}
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    transform.position += new Vector3(-1, 0, 0);
+        //}
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    transform.position += new Vector3(1, 0, 0);
+        //}
     }
 }
