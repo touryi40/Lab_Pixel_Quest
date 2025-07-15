18823e44a8cf9a61fda24196f9d90e70aa4eadfb
touryi40 <touryi2000@gmail.com>
2025-07-15 12:17:18 -0400

    game 7/15

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GeoController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public int speed = 5;
+    public string nextLevel = "Scene_2";
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)  
+        {
+            case "Death":
+                {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+        }
+        Debug.Log("Hit");
     }
 }
